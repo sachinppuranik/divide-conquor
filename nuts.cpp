20358c9282c877f26283456ba59a9836e320c1e4
root <sachin.puranik@honeywell.com>
2017-08-28 04:59:13 -0700

    working-copy

--- nuts.cpp
+++ nuts.cpp
@@ -1,26 +1,35 @@
 #include<iostream>
 using namespace std;
 
-int fun(int *arr,int low,int high)
+void fun(int *arr,int low,int high)
 {
 
-
-int mid = (low+high)/2;
-int left-mid-of-mid = mid/2;
-int right-mid-of-mid = mid + left-mid-of-mid;
+if(low == high)
+	return;
+int mid = (low+(high-low)/2)+1;
+if(mid == 0)
+	return;
+if(low == mid)
+	return;
+if(low + 1 == high)
+	return;
+int left_mid_of_mid = (low+mid)/2  ;
+int right_mid_of_mid = mid + left_mid_of_mid;
 //swap
-for(int i = left-mid-of-mid,int j=mid; i<=mid;i++,j++)
+for(int i = left_mid_of_mid,j=mid; i<mid;i++,j++)
 {
-  int temp = a[i];
-  a[i]=a[j];
-  a[j]=temp;
+  int temp = arr[i];
+  arr[i]=arr[j];
+  arr[j]=temp;
 }
-fun(arr,low,mid);
-fun(arr,mid+1,high);
+fun(arr,low,mid-1);
+fun(arr,mid,high);
 }
 int main()
 {
-int size = 8;
-int arr[size]={1,1,1,1,2,2,2,2};
-fun(arr,0,size);
+int size = 10;
+int arr[size]={1,1,1,1,1,2,2,2,2,2};
+fun(arr,0,size-1);
+for(auto i:arr)
+	cout<<i<<endl;
 }
